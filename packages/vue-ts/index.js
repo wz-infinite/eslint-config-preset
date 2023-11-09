97e1c4a5e40673b1da52a306624a8aa06d8502c6
atingchan <atingchan@163.com>
2023-11-09 17:38:39 +0800

    feat:vue-ts

--- packages/vue-ts/index.js
+++ packages/vue-ts/index.js
@@ -1,3 +1,23 @@
 module.exports = {
-  extends: [],
-}
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "@vue/standard",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["vue", "@typescript-eslint", "prettier"],
+  rules: {},
+};
